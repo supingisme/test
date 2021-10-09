8e0286fd043ea7d90cc107418119a7a4c69a4f0e
rtoax <2386499836@qq.com>
2021-10-09 13:10:03 +0800

    __attribute__((weak))

--- c/preprocessor/attribute/weak.c
+++ c/preprocessor/attribute/weak.c
@@ -5,7 +5,9 @@
 //给函数加上weak属性时，即使函数没定义，函数被调用也可以编译成功。
 //当有两个函数同名时，则使用强符号（也叫全局符号,即没有加weak的函数）来代替弱符号（加weak的函数）。
 
-int func() __weak;
+int __weak func() {
+	printf("weak function\n");
+}
 
 int main()
 {
