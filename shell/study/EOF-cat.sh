c349bd813fb678f695256c9c465bfd87ebbc73b6
rtoax <2386499836@qq.com>
2021-11-09 12:58:37 +0800

    cat > hello.c << EOF

--- shell/study/EOF-cat.sh
+++ shell/study/EOF-cat.sh
@@ -1,7 +1,20 @@
 #!/bin/bash 
 
+# demo 1
 cat<<'EOF'
 Line 1
 This is a pen;
 Hello world.
+EOF
+
+# demo2
+cat > hello.c << EOF
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+  printf("Hello world!\n");
+    sleep(999999999);
+}
 EOF
